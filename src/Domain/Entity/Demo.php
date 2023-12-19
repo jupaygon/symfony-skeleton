1ab9b1561cbc5b790ed3423976be67f94c928f10
Juanjo Payá <juanjopaya@gmail.com>
2023-12-19 02:05:40 +0100

    php bin/console make:entity '\App\Domain\Entity\Demo' ; move manually DemoRepository.php to src/Domain/Entity/

---
+++ src/Domain/Entity/Demo.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Domain\Entity;
+
+use App\Infrastructure\Database\Repository\DemoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DemoRepository::class)]
+class Demo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
